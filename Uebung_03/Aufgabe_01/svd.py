a59e929546b330f6e566098e6fc315535e2250f8
kahanor <karl.rielaender@gmail.com>
2018-10-30 17:14:04 +0100

    implemented orthogonal matrix using gram-schmidt

--- Uebung_03/Aufgabe_01/svd.py
+++ Uebung_03/Aufgabe_01/svd.py
@@ -1,11 +1,32 @@
 import numpy as np
 import numpy.linalg as la
 
+def gram_schmidt_cofficient(v1, v2):
+    return np.dot(v2, v1) / np.dot(v1, v1)
+
+
+def multiply(cofficient, v):
+    return map((lambda x : x * cofficient), v)
+
+
+def project(v1, v2):
+    return multiply(gram_schmidt_cofficient(v1, v2) , v1)
+
+
+def gram_schmidt(X):
+    B = []
+    for i in range(len(X)):
+        temp_vec = X[i]
+        for v in B :
+            proj_vec = project(v, X[i])
+            temp_vec = map(lambda x, y : x - y, temp_vec, proj_vec)
+        B.append(temp_vec)
+    return B
+
 def SVD(A):
-    At = np.transpose(A)
-    B = np.matmul(At, A)
+    B = np.matmul(A.T, A)
     ew, ev = la.eig(B)
-    # V = Orthonormale Basis aus den Eigenvektoren ev zu den Eigenwerten ew
+    V = gram_schmidt(ev)
     Vt = np.transpose(V)
     sv = np.sqrt(ew)
     Sigma = np.diag(sv)
